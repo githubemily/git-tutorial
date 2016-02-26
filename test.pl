0f5270ebbcd0455a0403b69a376ef234931f55e8
githubemily <githubemily@gmail.com>
2016-02-26 12:15:10 +0800

    Update test.pl

--- test.pl
+++ test.pl
@@ -1,1 +1,9 @@
-test
+
+if ($rt==JT::FALSE) {
+            put_log('ERROR',"\nVerification failed before doing failover...\n");
+            $clear->();
+            put_log("\ncheck the pfe fpc pic status...\n");
+            chk_fpc_pic_pfe($t, 'count' => 2) ? put_log("\nno problem for fpc pic pfe\n") : put_log('ERROR',"\ncheck fpc pic pfe failed\n");
+            return JT::FALSE;
+        }    
+end 
